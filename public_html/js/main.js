d539ea4488f6e7d410d43d1e74e3c02ca3f6dc3b
paul <paul.shibanov@gmail.com>
2013-12-16 01:00:37 +0600

    Part two Controllers

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,2 +1,18 @@
 var app = angular.module('myApp',[]);
-alert('Katamaran');
+app.run(function($rootScope){
+    $rootScope.name = "Paul";
+});
+app.controller('MyController',function($scope){
+    $scope.person = {
+        name: "Paul S."
+    };
+})
+app.controller('ParentController', function($scope) {
+  $scope.person = {greeted: false};
+});
+
+app.controller('ChildController', function($scope) {
+  $scope.sayHello = function() {
+    $scope.person.greeted = true;
+  }
+});
