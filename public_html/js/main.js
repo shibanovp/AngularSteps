0a298e5a2c3f6814bf8da4d5d560c454c268ca4a
paul <paul.shibanov@gmail.com>
2013-12-25 12:22:58 +0600

    Counter

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -13,4 +13,13 @@ app.controller('MyController',function($scope){
     	$scope.$apply(updateClock);
     },1);
     updateClock();
-})
+});
+app.controller('DemoController', function($scope){
+	$scope.counter = 0;
+	$scope.add = function(amount){
+		$scope.counter+=amount;
+	};
+	$scope.subtract = function(amount){
+		$scope.counter-=amount;
+	}
+});
